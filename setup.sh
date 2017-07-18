ef9d0e7b692b1348da2a2e1282d165e542500b12
Adam Liter <io@adamliter.org>
2017-07-17 23:02:43 -0400

    Use symlinks instead

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8; mode: sh; fill-column: 72; -*-
 
-for f in ./git-hooks/*; do
-    cp "${f}" "./.git/hooks/$(basename ${f})"
+for f in git-hooks/*; do
+    ln -sf "../../${f}" ".git/hooks/"
 done
